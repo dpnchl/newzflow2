3396b86f48eee2ac1b280f852e95120b91973419
newzflow <newzflow@ee2d39d8-02cc-32d8-d017-02425631ae01>
2010-11-18 21:46:31 +0000

    renamed sock.cpp to NntpSocket.cpp; added better error handling, status display to CDownloader/CNntpSocket/CConnectionView; also fixed app shutdown while downloader is in penalty loop.

--- NntpSocket.cpp
+++ NntpSocket.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include <time.h>
 #include "util.h"
-#include "sock.h"
+#include "NntpSocket.h"
 
 #ifdef _DEBUG
 #define new DEBUG_CLIENTBLOCK
@@ -147,7 +147,7 @@ CNntpSocket::CNntpSocket()
 : speed(3)
 {
 	recvBuffer.Create(10*1024);
-	s = INVALID_SOCKET;
+	sock = INVALID_SOCKET;
 
 	timeStart = timeEnd = 0;
 	bytesReceived = bytesSent = 0;
@@ -155,7 +155,7 @@ CNntpSocket::CNntpSocket()
 
 CNntpSocket::~CNntpSocket()
 {
-	if(s != INVALID_SOCKET)
+	if(sock != INVALID_SOCKET)
 		Close();
 }
 
@@ -172,8 +172,53 @@ void CNntpSocket::CloseWinsock()
 	WSACleanup();
 }
 
+void CNntpSocket::SetLastCommand(LPCTSTR fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	CComCritSecLock<CComAutoCriticalSection> lock(cs);
+	lastCommand.FormatV(fmt, args);
+	va_end(args);
+}
+
+void CNntpSocket::SetLastReply(LPCTSTR fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	CComCritSecLock<CComAutoCriticalSection> lock(cs);
+	lastReply.FormatV(fmt, args);
+	va_end(args);
+}
+
+CString CNntpSocket::GetLastError()
+{
+	int errCode = WSAGetLastError();
+
+	LPTSTR errString = NULL;  // will be allocated and filled by FormatMessage
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, 0, errCode, 0, (LPTSTR)&errString, 0, 0);
+
+	CString s(errString);
+	LocalFree(errString);
+
+	return s;
+}
+
+CString CNntpSocket::GetLastCommand()
+{
+	CComCritSecLock<CComAutoCriticalSection> lock(cs);
+	return lastCommand;
+}
+
+CString CNntpSocket::GetLastReply()
+{
+	CComCritSecLock<CComAutoCriticalSection> lock(cs);
+	return lastReply;
+}
+
 bool CNntpSocket::Connect(const CString& host, const CString& service, const CString& _user, const CString& _passwd)
 {
+	SetLastCommand(_T("Connecting to %s:%s..."), host, service);
+
 	// create socket
 	ADDRINFOT *result = NULL, hints;
 	ZeroMemory(&hints, sizeof(hints));
@@ -185,23 +230,28 @@ bool CNntpSocket::Connect(const CString& host, const CString& service, const CSt
 	int iResult = GetAddrInfo(host, service, &hints, &result);
 	ASSERT(iResult == 0);
 	if (iResult != 0) {
+		SetLastReply(_T("Connect failed: (GetAddrInfo) %s"), GetLastError());
 		return false;
 	}
+
+	CString reply;
 
 	// try to connect to all returned addresses
 	for(ADDRINFOT* ptr = result; ptr; ptr = ptr->ai_next) {
 		// Create a SOCKET for connecting to server
-		s = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-		ASSERT(s != INVALID_SOCKET);
-		if(s == INVALID_SOCKET) {
+		sock = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		ASSERT(sock != INVALID_SOCKET);
+		if(sock == INVALID_SOCKET) {
+			SetLastReply(_T("Connect failed: (sock) %s"), GetLastError());
 			FreeAddrInfo(result);
 			return false;
 		}
-		iResult = connect(s, ptr->ai_addr, (int)ptr->ai_addrlen);
+		iResult = connect(sock, ptr->ai_addr, (int)ptr->ai_addrlen);
 		if(iResult == SOCKET_ERROR) {
 			// connect failed, try next address
-			closesocket(s);
-			s = INVALID_SOCKET;
+			reply = GetLastError();
+			closesocket(sock);
+			sock = INVALID_SOCKET;
 		} else {
 			// connect succeeded
 			break;
@@ -211,7 +261,13 @@ bool CNntpSocket::Connect(const CString& host, const CString& service, const CSt
 	timeStart = time(NULL);
 	user = CStringA(_user);
 	passwd = CStringA(_passwd);
-	return s != INVALID_SOCKET;
+	if(sock == INVALID_SOCKET) {
+		SetLastReply(_T("Connect failed: (connect) %s"), reply);
+		return false;
+	} else {
+		SetLastReply(_T("Connected to %s:%s"), host, service);
+		return true;
+	}
 }
 
 CStringA CNntpSocket::ReceiveLine()
@@ -224,7 +280,7 @@ CStringA CNntpSocket::ReceiveLine()
 			recvBuffer.Consume(NULL, lineLen);
 			return line;
 		}
-		int iResult = recv(s, recvBuffer.GetFillPtr(), recvBuffer.GetFillSize(), 0);
+		int iResult = recv(sock, recvBuffer.GetFillPtr(), recvBuffer.GetFillSize(), 0);
 		if(iResult > 0) {
 			recvBuffer.Fill(NULL, iResult);
 			speed.OnReceive(iResult);
@@ -250,35 +306,45 @@ bool CNntpSocket::SendV(const char* fmt, va_list args)
 	line.FormatV(fmt, args);
 	va_end(args);
 	Util::Print(line);
-	{ CComCritSecLock<CComAutoCriticalSection> lock(cs);
-		lastCommand = line;
-	}
-	int iResult = send(s, line, line.GetLength(), 0);
+	int iResult = send(sock, line, line.GetLength(), 0);
 	bytesSent += iResult;
 	return iResult != SOCKET_ERROR;
 }
 
 CStringA CNntpSocket::Request(const char* fmt, ...)
 {
+	if(sock == INVALID_SOCKET)
+		return "";
+
+	CStringA command;
 	va_list args;
 	va_start(args, fmt);
+	command.FormatV(fmt, args);
+	va_end(args);
+	SetLastCommand(CString(command));
+
 again:
-	bool ret = SendV(fmt, args);
+	bool ret = Send(command);
 	if(!ret)
 		return "";
 	CStringA reply = ReceiveLine(); Util::Print(reply);
 	if(reply.IsEmpty())
 		return "";
+	SetLastReply(CString(reply));
 	if(reply.Left(3) == "480") {
 		if(user.IsEmpty() || passwd.IsEmpty())
 			return "";
 		Send("AUTHINFO USER %s\n", user); 
+		SetLastCommand(_T("AUTHINFO USER ***"));
 		reply = ReceiveLine(); Util::Print(reply);
+		SetLastReply(CString(reply));
 		if(reply.Left(3) == "281")
 			goto again;
 		if(reply.Left(3) == "381") {
 			Send("AUTHINFO PASS %s\n", passwd);
+			SetLastCommand(_T("AUTHINFO PASS ***"));
 			reply = ReceiveLine(); Util::Print(reply);
+			SetLastReply(CString(reply));
 			if(reply.Left(3) == "281")
 				goto again;
 			return "";
@@ -290,14 +356,17 @@ again:
 
 void CNntpSocket::Close()
 {
+	if(sock == INVALID_SOCKET)
+		return;
+
 	timeEnd = time(NULL);
 	CString str;
 	str.Format(_T("rate: %s (%s sent, %s received in %s)"), Util::FormatSpeed(__int64(bytesReceived + bytesSent) / __int64(timeEnd - timeStart)), Util::FormatSize(bytesSent), Util::FormatSize(bytesReceived), Util::FormatTimeSpan(timeEnd - timeStart));
 	Util::Print(CStringA(str));
 
-	shutdown(s, SD_SEND);
-	closesocket(s);
-	s = INVALID_SOCKET;
+	shutdown(sock, SD_SEND);
+	closesocket(sock);
+	sock = INVALID_SOCKET;
 }
 
 /*static*/ CSpeedMonitor CNntpSocket::totalSpeed(3);
@@ -360,4 +429,3 @@ void CSpeedMonitor::Trim()
 	while(!history.IsEmpty() && history.GetTail().first < now - slots)
 		history.RemoveTail();
 }
-
